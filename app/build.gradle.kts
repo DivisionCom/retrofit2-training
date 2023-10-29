c66bcbd38ed35373241334222a4f2e64ef228f00
DivisionCom <xd6511@gmail.com>
2023-10-29 11:22:30 +0300

    Add okHttp3 Dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,6 +42,9 @@ dependencies {
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
 
+    implementation("com.squareup.okhttp3:okhttp:4.7.2")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.7.2")
+
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
