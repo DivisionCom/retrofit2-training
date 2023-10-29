409eb821273022bef863480e0e4187b236c63a90
DivisionCom <xd6511@gmail.com>
2023-10-29 11:18:43 +0300

    Add getting product by ID

--- app/src/main/java/com/example/retrofit2_training/retrofit/ProductApi.kt
+++ app/src/main/java/com/example/retrofit2_training/retrofit/ProductApi.kt
@@ -1,8 +1,9 @@
 package com.example.retrofit2_training.retrofit
 
 import retrofit2.http.GET
+import retrofit2.http.Path
 
 interface ProductApi {
-    @GET("products/1")
-    suspend fun getProductById(): Product
+    @GET("products/{id}")
+    suspend fun getProductById(@Path("id") id: Int): Product
 }
