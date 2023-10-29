485e182084a33c364dbd3a7de0dc8538b30b030f
DivisionCom <xd6511@gmail.com>
2023-10-29 12:19:39 +0300

    Add okHttp client; http interceptor

--- app/src/main/java/com/example/retrofit2_training/MainActivity.kt
+++ app/src/main/java/com/example/retrofit2_training/MainActivity.kt
@@ -7,6 +7,8 @@ import com.example.retrofit2_training.retrofit.ProductApi
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
@@ -18,8 +20,16 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        val interceptor = HttpLoggingInterceptor()
+        interceptor.level = HttpLoggingInterceptor.Level.BODY
+
+        val client = OkHttpClient.Builder()
+            .addInterceptor(interceptor)
+            .build()
+
         val retrofit = Retrofit.Builder()
             .baseUrl("https://dummyjson.com/")
+            .client(client)
             .addConverterFactory(GsonConverterFactory.create()).build()
         val productApi = retrofit.create(ProductApi::class.java)
 
