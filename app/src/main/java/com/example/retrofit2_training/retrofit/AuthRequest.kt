86bfc1d86c8919129d1e859441d3966e205625e7
DivisionCom <xd6511@gmail.com>
2023-11-02 22:15:21 +0300

    Add data class AuthRequest & @POST req 'auth'

---
+++ app/src/main/java/com/example/retrofit2_training/retrofit/AuthRequest.kt
@@ -0,0 +1,6 @@
+package com.example.retrofit2_training.retrofit
+
+data class AuthRequest(
+    val username: String,
+    val password: String
+)
--- app/src/main/java/com/example/retrofit2_training/retrofit/MainApi.kt
+++ app/src/main/java/com/example/retrofit2_training/retrofit/MainApi.kt
@@ -1,9 +1,14 @@
 package com.example.retrofit2_training.retrofit
 
+import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.POST
 import retrofit2.http.Path
 
 interface MainApi {
     @GET("products/{id}")
     suspend fun getProductById(@Path("id") id: Int): Product
+
+    @POST("auth/login")
+    suspend fun auth(@Body authRequest: AuthRequest): User
 }
