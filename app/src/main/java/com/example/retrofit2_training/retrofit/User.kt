679cf83aaa60d231599b6537b5f2360ef4d4c726
DivisionCom <xd6511@gmail.com>
2023-11-02 22:10:29 +0300

    Add data class User

---
+++ app/src/main/java/com/example/retrofit2_training/retrofit/User.kt
@@ -0,0 +1,12 @@
+package com.example.retrofit2_training.retrofit
+
+data class User(
+    val id: Int,
+    val username: String,
+    val email: String,
+    val firstName: String,
+    val lastName: String,
+    val gender: String,
+    val image: String,
+    val token: String
+)
