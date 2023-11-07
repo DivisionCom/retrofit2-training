372d54cf325a23b9074081f75694cc7cfa3b9516
DivisionCom <xd6511@gmail.com>
2023-11-07 19:09:26 +0300

    Add retrofit request GET searching products

--- app/src/main/java/com/example/retrofit2_training/retrofit/MainApi.kt
+++ app/src/main/java/com/example/retrofit2_training/retrofit/MainApi.kt
@@ -4,10 +4,14 @@ import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.POST
 import retrofit2.http.Path
+import retrofit2.http.Query
 
 interface MainApi {
     @GET("products/{id}")
     suspend fun getProductById(@Path("id") id: Int): Product
+
+    @GET("products/search")
+    suspend fun getSearchProducts(@Query("q") searchName: String): AllProducts
 
     @GET("products")
     suspend fun getAllProducts(): AllProducts
