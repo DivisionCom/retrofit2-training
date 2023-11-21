6c00f92af93f44861138c3849951bac0be543465
DivisionCom <xd6511@gmail.com>
2023-11-21 21:59:09 +0300

    Add CheckUser function
    Add fun filling interface with user data, Add error text if Data incorrect

--- app/src/main/java/com/example/retrofit2_training/AuthFragment.kt
+++ app/src/main/java/com/example/retrofit2_training/AuthFragment.kt
@@ -10,11 +10,15 @@ import androidx.navigation.fragment.findNavController
 import com.example.retrofit2_training.databinding.FragmentAuthBinding
 import com.example.retrofit2_training.retrofit.AuthRequest
 import com.example.retrofit2_training.retrofit.MainApi
+import com.example.retrofit2_training.retrofit.User
+import com.squareup.picasso.Picasso
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
+import org.json.JSONObject
+import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
@@ -35,22 +39,47 @@ class AuthFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         initRetrofit()
 
+        checkUser()
+        login()
+    }
+
+    private fun checkUser() {
         binding.btnCheck.setOnClickListener {
             auth(
                 AuthRequest(
-                    binding.btnLogin.text.toString(),
+                    binding.etLogin.text.toString(),
                     binding.etPassword.text.toString()
                 )
             )
         }
-
-
-        login()
     }
 
     private fun auth(authRequest: AuthRequest) {
         CoroutineScope(Dispatchers.IO).launch {
             val response = mainApi.auth(authRequest)
+            fillViews(response)
+            invalidData(response)
+        }
+    }
+
+    private fun fillViews(response: Response<User>) {
+        val user = response.body()
+        requireActivity().runOnUiThread {
+            if(user != null) {
+                Picasso.get().load(user.image).into(binding.ivUser)
+                binding.tvUserName.text = "${user.firstName} ${user.lastName}"
+                binding.btnLogin.isEnabled = true
+                authViewModel.token.value = user.token
+            }
+        }
+    }
+
+    private fun invalidData(response: Response<User>){
+        val message = response.errorBody()?.string()?.let {
+            JSONObject(it).getString("message")
+        }
+        requireActivity().runOnUiThread {
+            binding.tvError.text = message
         }
     }
 
